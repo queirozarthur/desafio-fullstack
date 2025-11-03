348f7d885bc5ed6037ad4cab536199216079934b
queirozarthur <queirozarthur2109@gmail.com>
2025-11-03 16:12:31 -0300

    esqueleto CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FuncionariosModule } from './funcionarios/funcionarios.module';
 
 @Module({
-  imports: [],
+  imports: [FuncionariosModule],
   controllers: [AppController],
   providers: [AppService],
 })
